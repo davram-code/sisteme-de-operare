e97d44ca3ae5158f8730ba75d7bccf83c62fffbe
dan.avram <dan.avram@mta.ro>
2023-09-03 18:06:46 +0300

    Replace tabs with spaces

--- examene/2023-08-31/scriptul1.sh
+++ examene/2023-08-31/scriptul1.sh
@@ -1,99 +1,99 @@
 #!/bin/bash
 function can_login() {
-	password=$(sudo grep -E "^$1:" /etc/shadow | cut -f2 -d:)
-	
-	if [[ "$password" == "!" ||  "$password" == "!!" || "$password" == "*"  ]]
-	then
-		echo "Utilizatoru $1 nu are parolă setată. Doriți să setați una? (y/n)"
-		read ans
+    password=$(sudo grep -E "^$1:" /etc/shadow | cut -f2 -d:)
+    
+    if [[ "$password" == "!" ||  "$password" == "!!" || "$password" == "*"  ]]
+    then
+        echo "Utilizatoru $1 nu are parolă setată. Doriți să setați una? (y/n)"
+        read ans
 
-		if [[ "$ans" = "y" || "$ans" = "Y" ]]
-		then 
-			sudo passwd $1
-		fi
-		return 1
-	fi
-		
-	if echo -n $password | grep -Eq "!.+"
-	then
-		echo "Utilizatoru $1 este blocat. Doriți să îl deblocați? (y/n)"
-		read ans
+        if [[ "$ans" = "y" || "$ans" = "Y" ]]
+        then 
+            sudo passwd $1
+        fi
+        return 1
+    fi
+        
+    if echo -n $password | grep -Eq "!.+"
+    then
+        echo "Utilizatoru $1 este blocat. Doriți să îl deblocați? (y/n)"
+        read ans
 
-		if [[ "$ans" = "y" || "$ans" = "Y" ]]
-		then 
-			sudo usermod -U $1
-		fi
-		return 1
-	fi
+        if [[ "$ans" = "y" || "$ans" = "Y" ]]
+        then 
+            sudo usermod -U $1
+        fi
+        return 1
+    fi
 
-	return 0
+    return 0
 }
 
 if [[ $# -eq 1 ]]
 then 
-	if grep -Eq "^$1:" /etc/passwd
-	then 
-		if can_login $1
-		then
-			echo "user is not blocked and has paasword set"
-		fi
-	else
-		echo "User does not exist"
-	fi
+    if grep -Eq "^$1:" /etc/passwd
+    then 
+        if can_login $1
+        then
+            echo "user is not blocked and has paasword set"
+        fi
+    else
+        echo "User does not exist"
+    fi
 fi
 
 if [[ $# -eq 2 ]]
 then 
-	if [[ "$1" = "--proc" || "$2" = "--proc" ]]
-	then
-		values=$(ps -u "$1" -o vsz | tail -n+1)
-		declare -i sum=0
+    if [[ "$1" = "--proc" || "$2" = "--proc" ]]
+    then
+        values=$(ps -u "$1" -o vsz | tail -n+1)
+        declare -i sum=0
 
-		for i in $values
-		do
-			sum+=$i
-		done
+        for i in $values
+        do
+            sum+=$i
+        done
 
-		ps -u $1 
-	fi
+        ps -u $1 
+    fi
 
-	if grep -Eq "^$1:" /etc/passwd && grep -Eq "^$2:" /etc/passwd
-	then
-		sudo groupadd studenti
-		sudo usermod -aG studenti $1
-		sudo usermod -aG studenti $2
+    if grep -Eq "^$1:" /etc/passwd && grep -Eq "^$2:" /etc/passwd
+    then
+        sudo groupadd studenti
+        sudo usermod -aG studenti $1
+        sudo usermod -aG studenti $2
 
-		echo "Cale director: "
-		read dir
+        echo "Cale director: "
+        read dir
 
-		for file in $dir/*
-		do
-			owner=$(stat -c%u $file)
-			if [[ -f "$file" && "$owner" = "$1" ]]
-			then
-				sudo chown "$2" $file
-			fi
-		done
-	fi
+        for file in $dir/*
+        do
+            owner=$(stat -c%u $file)
+            if [[ -f "$file" && "$owner" = "$1" ]]
+            then
+                sudo chown "$2" $file
+            fi
+        done
+    fi
 fi
 
 if [[ $# -ge 2 ]]
 then 
-	for user in $@
-	do
-		if grep -Eq "^$user:" /etc/passwd
-		then
-			name=$user
-			home=$(grep -E "^$user:" /etc/passwd | cut -f6 -d: )
-			grup_pp=$(id -ng "$user")
-			shell=$(grep -E "^$user:" /etc/passwd | cut -f7 -d: )
+    for user in $@
+    do
+        if grep -Eq "^$user:" /etc/passwd
+        then
+            name=$user
+            home=$(grep -E "^$user:" /etc/passwd | cut -f6 -d: )
+            grup_pp=$(id -ng "$user")
+            shell=$(grep -E "^$user:" /etc/passwd | cut -f7 -d: )
 
-			if [[ -n "$shell" ]]
-			then
-				echo "$name\$$home\$$grup_pp\$$shell"
-			else 
-				echo "$name\$$home\$$grup_pp"
-			fi
-		fi
-	done
+            if [[ -n "$shell" ]]
+            then
+                echo "$name\$$home\$$grup_pp\$$shell"
+            else 
+                echo "$name\$$home\$$grup_pp"
+            fi
+        fi
+    done
 fi
--- examene/2023-08-31/scriptul2.sh
+++ examene/2023-08-31/scriptul2.sh
@@ -20,185 +20,184 @@ declare -i file_dim=0
 
 # This could be hardcoded, but this is nicer :)
 function parse_args() {
-	for arg in $@
-	do
-		case $arg in
-			'-q') quiet=1 ;;
-			'--init') init=1 ;;
-			'--check-files') check_files=1 ;;
-			'--check-metadata') check_meta=1 ;;
-			'--check-full') check_full=1 ;;
-			*)  
-				# If we have --check-files or --check-meta we only have one file and the dir
-				if [[ $check_files = 1 || $check_meta = 1 ]]
-				then 
-					if [[ -z "$meta_file" ]]
-					then
-						meta_file=$arg
-					else 
-						dir=$arg
-					fi
-				fi
+    for arg in $@
+    do
+        case $arg in
+            '-q') quiet=1 ;;
+            '--init') init=1 ;;
+            '--check-files') check_files=1 ;;
+            '--check-metadata') check_meta=1 ;;
+            '--check-full') check_full=1 ;;
+            *)  
+                # If we have --check-files or --check-meta we only have one file and the dir
+                if [[ $check_files = 1 || $check_meta = 1 ]]
+                then 
+                    if [[ -z "$meta_file" ]]
+                    then
+                        meta_file=$arg
+                    else 
+                        dir=$arg
+                    fi
+                fi
 
-				if [[ $check_full = 1 || $init = 1 ]]
-				then 
-					if [[ -z "$checksum_file" ]]
-					then
-						checksum_file=$arg
-					elif [[ -z "$meta_file" ]]
-					then
-						meta_file=$arg
-					else
-						dir=$arg
-					fi
-				fi
-				;;
-		esac
-	done
+                if [[ $check_full = 1 || $init = 1 ]]
+                then 
+                    if [[ -z "$checksum_file" ]]
+                    then
+                        checksum_file=$arg
+                    elif [[ -z "$meta_file" ]]
+                    then
+                        meta_file=$arg
+                    else
+                        dir=$arg
+                    fi
+                fi
+                ;;
+        esac
+    done
 
 }
 
 function init() {
-	cd $dir
+    cd $dir
 
-	# Iterate through files in given directory. 
-	# Could also be done with "files=$(ls -R test | tr -d ':' | sed '/\./d' )" but this is better
-	find . | while read file 
-	do
+    # Iterate through files in given directory. 
+    # Could also be done with "files=$(ls -R test | tr -d ':' | sed '/\./d' )" but this is better
+    find . | while read file 
+    do
 
-		# Get file type
-		file_type=$(ls -ld $file | head -c1)
+        # Get file type
+        file_type=$(ls -ld $file | head -c1)
 
-		if [[ "$file_type" = "-" ]]
-		then
-			checksum=$(sha256sum "$file" | cut -f1 -d" ")
-			echo "$file,$file_type,$checksum" >> $checksum_file
-		elif [[ "$file_type" = "l" ]]
-		then
-			dest_name=$(ls -ld $file | rev | cut -f1 -d " " | rev)
-			echo "$file,$file_type,$dest_name" >> $checksum_file
-		else
-			echo "$file,$file_type," >> $checksum_file
-		fi
+        if [[ "$file_type" = "-" ]]
+        then
+            checksum=$(sha256sum "$file" | cut -f1 -d" ")
+            echo "$file,$file_type,$checksum" >> $checksum_file
+        elif [[ "$file_type" = "l" ]]
+        then
+            dest_name=$(ls -ld $file | rev | cut -f1 -d " " | rev)
+            echo "$file,$file_type,$dest_name" >> $checksum_file
+        else
+            echo "$file,$file_type," >> $checksum_file
+        fi
 
-		# Metadata file
-		name=$file
-		dim=$(stat -c%s $file)
-		modif=$(stat -c%y $file)
-		perm=$(stat -c%a $file)
-		owner=$(stat -c%U $file)
-		gowner=$(stat -c%G $file)
+        # Metadata file
+        name=$file
+        dim=$(stat -c%s $file)
+        modif=$(stat -c%y $file)
+        perm=$(stat -c%a $file)
+        owner=$(stat -c%U $file)
+        gowner=$(stat -c%G $file)
 
-		echo "$name,$dim,$modif,$perm,$owner,$gowner" >> $meta_file
-	done
-	
-	cd - > /dev/null
+        echo "$name,$dim,$modif,$perm,$owner,$gowner" >> $meta_file
+    done
+    
+    cd - > /dev/null
 
-	return 0
+    return 0
 }
 
 parse_args $@
 
-
 # If relative paths, convert to absolute paths
 if [[ -n $meta_file && ${meta_file:0:1} != '/' ]]
 then
-	meta_file=$(realpath $meta_file)
+    meta_file=$(realpath $meta_file)
 fi
 if [[ -n $checksum_file && ${checksum_file:0:1} != '/' ]]
 then
-	checksum_file=$(realpath $checksum_file)
+    checksum_file=$(realpath $checksum_file)
 fi
 
 
 if [[ $init = 1 ]]
 then
-	# Remove files if they already exist
-	[[ -f "$meta_file" ]] && rm $meta_file
-	[[ -f "$checksum_file" ]] && rm $checksum_file
-	init
+    # Remove files if they already exist
+    [[ -f "$meta_file" ]] && rm $meta_file
+    [[ -f "$checksum_file" ]] && rm $checksum_file
+    init
 else
-	cd $dir
-	while read line
-	do
-		file=$(echo $line | cut -f1 -d,)
-		if [[ ! -e "$file" ]]
-		then
-			[[ $quiet = 0 ]] && echo $file does not exist!
-			return_code=1
+    cd $dir
+    while read line
+    do
+        file=$(echo $line | cut -f1 -d,)
+        if [[ ! -e "$file" ]]
+        then
+            [[ $quiet = 0 ]] && echo $file does not exist!
+            return_code=1
 
-			# If not exist, we shouldn't check any further
-			continue
-		fi
+            # If not exist, we shouldn't check any further
+            continue
+        fi
 
-		if [[ $check_files = 1 ]]
-		then
-			continue
-		fi
+        if [[ $check_files = 1 ]]
+        then
+            continue
+        fi
 
-		dim=$(stat -c%s $file)
-		modif=$(stat -c%y $file)
-		perm=$(stat -c%a $file)
-		owner=$(stat -c%U $file)
-		gowner=$(stat -c%G $file)
+        dim=$(stat -c%s $file)
+        modif=$(stat -c%y $file)
+        perm=$(stat -c%a $file)
+        owner=$(stat -c%U $file)
+        gowner=$(stat -c%G $file)
 
-		check_dim=$(echo $line | cut -f2 -d,)
-		check_modif=$(echo $line | cut -f3 -d,)
-		check_perm=$(echo $line | cut -f4 -d,)
-		check_owner=$(echo $line | cut -f5 -d,)
-		check_gowner=$(echo $line | cut -f6 -d,)
+        check_dim=$(echo $line | cut -f2 -d,)
+        check_modif=$(echo $line | cut -f3 -d,)
+        check_perm=$(echo $line | cut -f4 -d,)
+        check_owner=$(echo $line | cut -f5 -d,)
+        check_gowner=$(echo $line | cut -f6 -d,)
 
-		if [[ "$dim" !=  $check_dim ]]
-		then 
-			# if -q is not set, print info
-			[[ $quiet = 0 ]] && echo $file incorrect dimension $dim, expected $check_dim
-			return_code=1
-		fi
+        if [[ "$dim" !=  $check_dim ]]
+        then 
+            # if -q is not set, print info
+            [[ $quiet = 0 ]] && echo $file incorrect dimension $dim, expected $check_dim
+            return_code=1
+        fi
 
-		if [[ "$modif" !=  "$check_modif" ]]
-		then 
-			[[ $quiet = 0 ]] && echo $file incorrect modif time $modif, expected $check_modif
-			return_code=1
-		fi
+        if [[ "$modif" !=  "$check_modif" ]]
+        then 
+            [[ $quiet = 0 ]] && echo $file incorrect modif time $modif, expected $check_modif
+            return_code=1
+        fi
 
-		if [[ "$perm" !=  "$check_perm" ]]
-		then 
-			[[ $quiet = 0 ]] && echo $file incorrect perm $perm, expected $check_perm
-			return_code=1
-		fi
+        if [[ "$perm" !=  "$check_perm" ]]
+        then 
+            [[ $quiet = 0 ]] && echo $file incorrect perm $perm, expected $check_perm
+            return_code=1
+        fi
 
-		if [[ "$owner" !=  "$check_owner" ]]
-		then 
-			[[ $quiet = 0 ]] && echo $file incorrect owner $owner, expected $check_owner
-			return_code=1
-		fi
+        if [[ "$owner" !=  "$check_owner" ]]
+        then 
+            [[ $quiet = 0 ]] && echo $file incorrect owner $owner, expected $check_owner
+            return_code=1
+        fi
 
-		if [[ "$gowner" !=  "$check_gowner" ]]
-		then 
-			[[ $quiet = 0 ]] && echo $file incorrect gowner $gowner, expected $check_gowner
-			return_code=1
-		fi
+        if [[ "$gowner" !=  "$check_gowner" ]]
+        then 
+            [[ $quiet = 0 ]] && echo $file incorrect gowner $gowner, expected $check_gowner
+            return_code=1
+        fi
 
-		if [[ $check_meta = 1 ]]
-		then
-			continue
-		fi
+        if [[ $check_meta = 1 ]]
+        then
+            continue
+        fi
 
-		file_type=$(grep -E "^$file," $checksum_file|cut -f2 -d,)
-		if [[ "$file_type" = "-" ]]
-		then
-			checksum=$(sha256sum "$file"  | cut -f1 -d" ")
-			check_checksum=$(grep -E "^$file," $checksum_file|cut -f3 -d,)
+        file_type=$(grep -E "^$file," $checksum_file|cut -f2 -d,)
+        if [[ "$file_type" = "-" ]]
+        then
+            checksum=$(sha256sum "$file"  | cut -f1 -d" ")
+            check_checksum=$(grep -E "^$file," $checksum_file|cut -f3 -d,)
 
-			if [[ "$checksum" != "$check_checksum" ]]
-			then
-				[[ $quiet = 0 ]] && echo $file: incorrect checksum $checksum, expected $check_checksum
-				return_code=1
-			fi
-		fi
+            if [[ "$checksum" != "$check_checksum" ]]
+            then
+                [[ $quiet = 0 ]] && echo $file: incorrect checksum $checksum, expected $check_checksum
+                return_code=1
+            fi
+        fi
 
-	done < $meta_file
-	cd - >/dev/null
+    done < $meta_file
+    cd - >/dev/null
 fi
 
 # To check running time
@@ -212,7 +211,7 @@ stop=$(date +%s)
 file_no=$(wc -l $meta_file |cut -f1 -d" ")
 for size in $(cut -f2 -d, $meta_file)
 do
-	file_dim+=$size
+    file_dim+=$size
 done
 
 run_time=$(($stop-$start))
